181d3ce1d718b2319ba620af301ad0ac6333a6a3
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-06-16 22:21:14 +0545

    June 16 commits

--- src/StudentAgeName/RollComparator.java
+++ src/StudentAgeName/RollComparator.java
@@ -6,6 +6,7 @@ public class RollComparator implements Comparator<Student> {
 
     @Override
     public int compare(Student o1, Student o2) {
-        return o1.getRoll()- o2.getRoll();
+        return Integer.compare(o2.getRoll(), o1.getRoll());
+
     }
 }
--- src/StudentAgeName/Test.java
+++ src/StudentAgeName/Test.java
@@ -32,37 +32,16 @@ public class Test {
         for (int i = 0; i < students.size(); i++) {
             System.out.println(students.get(i));
         }
-
+        System.out.println("Sorting according Roll Number: ");
         Collections.sort(students, new RollComparator());
         for(Student st : students){
             System.out.println(st.getRoll()+ " : "+st.getName());
         }
+        System.out.println("Sorting according Names: ");
         Collections.sort(students, new NameComparator());
         for(Student st : students){
             System.out.println(st.getRoll()+ " : "+st.getName());
         }
 
-
-
-
-
-//        List<String> rolls = new ArrayList<>();
-
-//        for(Student student : students){
-//            System.out.println(student.get(i));
-//            rolls.add(student.getName());
-//        }
-//        StringBuffer temp = new StringBuffer();
-//        for (int i = 0; i < rolls.size(); i++) {
-//            for (int j = 0; j < rolls.size()-1-i; j++) {
-//                if(rolls.get(j).equals(rolls.get(j+1))){
-//                    temp.append(rolls.get(j));
-//                    rolls.set(j, rolls.get(j+1));
-//                    rolls.set((j+1), temp);
-//                    temp.
-//                }
-//            }
-//        }
-//        System.out.println(rolls);
     }
 }
